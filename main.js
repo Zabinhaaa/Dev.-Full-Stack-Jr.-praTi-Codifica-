4e9d63c449a33409e44bd250e215eb46c0d545b3
Zabinhaaa <pedrozabaoficial@gmail.com>
2026-05-24 23:55:16 -0300

    Faltam dois exercícios

--- main.js
+++ main.js
@@ -124,5 +124,127 @@ function question5() {
 }
 
 function question6() {
-    rl.question
+    rl.question("Que valor deseja sacar?", (resposta) => {
+        let valorSaque = parseInt(resposta);
+
+        if (valorSaque % 10 !== 0) {
+            console.log("Digite um valor múltiplo de 10");
+            rl.close();
+            return;
+        }
+
+        let cedulas100 = parseInt(valorSaque / 100);
+            valorSaque = valorSaque % 100;
+
+        let cedulas50 = parseInt(valorSaque / 50);
+            valorSaque = valorSaque % 50;
+
+        let cedulas20 = parseInt(valorSaque / 20);
+            valorSaque = valorSaque % 20;
+
+        let cedulas10 = parseInt(valorSaque / 10);
+            valorSaque = valorSaque % 10;
+            
+        console.log("Cédulas de R$ 100:", cedulas100)
+        console.log("Cédulas de R$ 50:", cedulas50)
+        console.log("Cédulas de R$ 20:", cedulas20)
+        console.log("Cédulas de R$ 10:", cedulas10)
+
+        question7();
+    });
+}
+
+function question7() {
+    rl.question("DIgite o primeiro número", (resposta) => {
+        let numero1 = parseFloat(resposta);
+
+        rl.question("Digite o segundo número",(resposta) => {
+            let numero2 = parseFloat(resposta);
+
+            rl.question("Digite a operação (+, -, *, /):", (resposta) => {
+                console.log(resposta);
+                let operacao = resposta 
+                let resultado;
+
+                switch (operacao) {
+                    case "+":
+                        resultado = numero1 + numero2;
+                        console.log("Resultado:", resultado);
+                        break;
+
+                    case "-":
+                        resultado = numero1 - numero2;
+                        console.log("Resultado:", resultado);
+                        break;
+
+                    case "*":
+                        resultado = numero1 * numero2;
+                        console.log("Resultado:", resultado);
+                        break;
+
+                    case "/":
+                        if (numero2 === 0) {
+                            console.log("Não é possível dividir por zero.");
+                        } else {
+                            resultado = numero1 / numero2;
+                            console.log("Resultado:", resultado);
+                        }
+                        break;
+
+                    default:
+                        console.log("operação inválida");
+                }
+                        
+                question8();
+
+            });
+        });
+    });
+}
+
+function question8() {
+    rl.question("Digite o tamanho da pizza (P, M, G): ", (resposta) => {
+        let tamanho = resposta.toUpperCase();
+
+        let precoBase;
+
+        switch (tamanho) {
+            case "P":
+            precoBase = "R$ 25";
+            break;
+
+        case "M":
+            precoBase = "R$ 35";
+            break;
+
+        case "G":
+            precoBase = "R$ 45";
+            break;
+
+        default:
+            console.log("Tamanho inválido!");
+            rl.close  
+            return;
+        }
+
+        rl.question("Digite o tipo de borda (Tradicional ou recheada):", (resposta) => {
+            let borda = resposta.toLowerCase();
+
+        let valorBorda;
+
+        if (borda === "tradicional") {
+                valorBorda = "R$ 0";
+        } else if (borda === "recheada") {
+            valorBorda = "R$ 10";
+        } else {
+            console.log("Tipo de borda inválida!");
+            rl.close();
+            return;
+        }
+
+        let valorFinal = precoBase + valorBorda;
+        console.log("valor final da pizza: R$", valorFinal);
+        
+       });
+   });
 }
